89c859d3e510cd9eb78e0872840a49f85407a63f
Grant Scott <grantscott.phd@gmail.com>
2018-01-23 23:10:06 -0600

    what happened... this adds the missing function

--- hw1/src/hw1.cpp
+++ hw1/src/hw1.cpp
@@ -97,3 +97,15 @@ int main(int argc, char * argv[])
 
 	return 0;
 }
+
+void initRandomMatrix(scottgs::FloatMatrix& m)
+{
+	// Initialize each element.
+	// See discussion board for better way, 
+	// this was originally posted to be a 
+	// simple example of per-element access into the matric
+	for (unsigned i = 0; i < m.size1(); ++ i)
+	        for (unsigned j = 0; j < m.size2(); ++ j)
+		            m (i, j) = (static_cast<float>(rand()) / RAND_MAX) * 100.0;
+}
+
